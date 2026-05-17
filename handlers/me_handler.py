6b3daa5b8fe58571d1d2cffd8632222576ebab21
Tuğberk Özdemir <tugberkozdemir@gmail.com>
2026-05-17 16:48:20 +0300

    OTP Request debug

--- handlers/me_handler.py
+++ handlers/me_handler.py
@@ -10,32 +10,33 @@ from utils.database_connection import MySQLClient
 class MeHandler:
 
     def __init__(self):
-
         self.db = MySQLClient()
 
         self.raw_token: str = ""
         self.hashed_token: str = ""
 
         self.user_id: Optional[int] = None
+        self.user_email: Optional[str] = None
 
         self.most_recent_backup: Optional[str] = None
 
-    async def find_user_id(self):
-
+    async def find_user_info(self):
         record = await self.db.fetch_one(
-            query = "SELECT * FROM tokens WHERE token_hash = %s",
+            query="SELECT * FROM tokens WHERE token_hash = %s",
             params=(self.hashed_token,)
         )
 
         self.user_id = record["user_id"]
-
+        self.user_email = record["email"]
 
     async def get_most_recent_backup(self):
-
         record = await self.db.fetch_one(
-            query = """
-                SELECT * FROM backups WHERE user_id = %s ORDER BY created_at DESC LIMIT 1
-            """,
+            query="""
+                  SELECT *
+                  FROM backups
+                  WHERE user_id = %s
+                  ORDER BY created_at DESC LIMIT 1
+                  """,
             params=(self.user_id,)
         )
 
@@ -43,15 +44,14 @@ class MeHandler:
 
         self.most_recent_backup = record["created_at"]
 
-
     async def handle(self, request: Request) -> JSONResponse:
-
         payload = await request.json()
 
         self.raw_token = payload["token"]
         self.hashed_token = hashlib.sha256(self.raw_token.encode()).hexdigest()
 
+        await self.find_user_info()
+
         await self.get_most_recent_backup()
 
         return JSONResponse(status_code=200, content={"most_recent_backup": self.most_recent_backup})
-
